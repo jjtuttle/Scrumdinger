2f94cfdc9264c10cd6109823b539fc5a05f9c886
James Tuttle <james.j.tuttle@gmail.com>
2025-08-20 12:44:00 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by James Tuttle on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
