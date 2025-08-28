987ff61c2a0f4fdf63a7d0f3e3eba80b6ade6185
James Tuttle <james.j.tuttle@gmail.com>
2025-08-28 08:02:40 -0700

    Added Card View and labels for the scrum struct data.

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,30 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by James Tuttle on 8/28/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+            }
+        }
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 400, height: 60)) {
+    let scrum = DailyScrum.sampleData[0]
+    return CardView(scrum: scrum)
+        .background(scrum.theme.mainColor)
+}
