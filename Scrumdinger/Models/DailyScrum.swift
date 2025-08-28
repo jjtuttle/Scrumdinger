bd64d3445adfb49ca91dc75efa699016d3e3a60e
James Tuttle <james.j.tuttle@gmail.com>
2025-08-28 07:37:52 -0700

    Added themeKit, Model, and sample data.

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,17 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by James Tuttle on 8/28/25.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
---
+++ Scrumdinger/Preview Content/DailyScrum+Sample.swift
@@ -0,0 +1,29 @@
+//
+//  DailyScrum+Sample.swift
+//  Scrumdinger
+//
+//  Created by James Tuttle on 8/28/25.
+//
+
+import Foundation
+import ThemeKit
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design",
+                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                   lengthInMinutes: 10,
+                   theme: .yellow
+                  ),
+        DailyScrum(title: "App Dev",
+                   attendees: ["Katie","Gray","Enua","Lauis","Darla"],
+                   lengthInMinutes: 5,
+                   theme: .orange
+                  ),
+        DailyScrum(title: "Web Dev",
+                   attendees: ["Chella","Chris","Christina","Eden","Karla","Lindsey","Aga","Chad","Jenn","Sarah"],
+                   lengthInMinutes: 5,
+                   theme: .poppy)
+    ]
+}
